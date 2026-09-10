973a48e40802b2531ccd818f50ec6161cb5508de
virendrahardikrviru-commits <virendra.hardikrviru@gmail.com>
2026-09-10 16:58:32 +0530

    Phase 1: Rebuild walker-nav-menu - new class name, lct- classes, accessible toggles

--- inc/class-listingcore-theme-walker-nav-menu.php
+++ inc/class-listingcore-theme-walker-nav-menu.php
@@ -1,70 +1,125 @@
 <?php
 /**
- * Custom Nav Menu Walker
+ * Custom nav menu walker.
  *
- * @package ClassiPressPro
+ * Extends Walker_Nav_Menu with custom markup for the theme's menus.
+ *
+ * @package ListingCoreTheme
+ * @since   1.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
 
-class ClassiPress_Walker_Nav_Menu extends Walker_Nav_Menu {
+/**
+ * Custom Walker for nav menus.
+ */
+class ListingCore_Theme_Walker_Nav_Menu extends Walker_Nav_Menu {
 
-    public function start_lvl( &$output, $depth = 0, $args = null ) {
-        $indent  = str_repeat( "\t", $depth );
-        $output .= "\n$indent<ul class=\"sub-menu cp-dropdown\" role=\"menu\">\n";
-    }
+	/**
+	 * Starts the list before the elements are added.
+	 *
+	 * @param string   $output Passed by reference. Used to append additional content.
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = null ) {
+		$indent  = str_repeat( "\t", $depth );
+		$classes = [ 'lct-submenu', 'lct-submenu--depth-' . absint( $depth + 1 ) ];
 
-    public function start_el( &$output, $data_object, $depth = 0, $args = null, $id = 0 ) {
-        $item   = $data_object;
-        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+		if ( 0 === $depth ) {
+			$classes[] = 'lct-submenu--top';
+		}
 
-        $classes   = empty( $item->classes ) ? [] : (array) $item->classes;
-        $classes[] = 'menu-item-' . $item->ID;
+		$class_names = implode( ' ', $classes );
 
-        if ( in_array( 'menu-item-has-children', $classes, true ) ) {
-            $classes[] = 'cp-has-dropdown';
-        }
+		$output .= "\n{$indent}<ul class=\"" . esc_attr( $class_names ) . "\">\n";
+	}
 
-        $class_names = implode( ' ', array_filter( array_map( 'esc_attr', $classes ) ) );
-        $id          = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args, $depth );
-        $id          = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+	/**
+	 * Ends the list of items after the elements are added.
+	 *
+	 * @param string   $output Passed by reference. Used to append additional content.
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
+	public function end_lvl( &$output, $depth = 0, $args = null ) {
+		$indent  = str_repeat( "\t", $depth );
+		$output .= "{$indent}</ul>\n";
+	}
 
-        $output .= $indent . '<li' . $id . ' class="' . $class_names . '">';
+	/**
+	 * Starts the element output.
+	 *
+	 * @param string   $output Passed by reference. Used to append additional content.
+	 * @param WP_Post  $item   Menu item data object.
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 * @param int      $id     Current item ID.
+	 */
+	public function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+		$indent = $depth ? str_repeat( "\t", $depth ) : '';
 
-        $atts          = [];
-        $atts['title'] = ! empty( $item->attr_title ) ? $item->attr_title : '';
-        $atts['target']= ! empty( $item->target ) ? $item->target : '';
-        $atts['rel']   = ! empty( $item->xfn ) ? $item->xfn : '';
-        $atts['href']  = ! empty( $item->url ) ? $item->url : '';
-        $atts['class'] = 'menu-link';
+		$classes   = empty( $item->classes ) ? [] : (array) $item->classes;
+		$classes[] = 'lct-menu__item';
+		$classes[] = 'lct-menu__item--depth-' . absint( $depth );
 
-        if ( $item->current || $item->current_item_ancestor ) {
-            $atts['aria-current'] = 'page';
-        }
+		if ( in_array( 'menu-item-has-children', $classes, true ) ) {
+			$classes[] = 'lct-menu__item--has-children';
+		}
 
-        $atts       = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
-        $attributes = '';
-        foreach ( $atts as $attr => $value ) {
-            if ( is_scalar( $value ) && '' !== $value && false !== $value ) {
-                $value       = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-                $attributes .= ' ' . $attr . '="' . $value . '"';
-            }
-        }
+		if ( in_array( 'current-menu-item', $classes, true ) || in_array( 'current-menu-parent', $classes, true ) ) {
+			$classes[] = 'lct-menu__item--current';
+		}
 
-        $title = apply_filters( 'the_title', $item->title, $item->ID );
-        $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+		$class_names = implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
 
-        $item_output  = $args->before ?? '';
-        $item_output .= '<a' . $attributes . '>';
-        $item_output .= ( $args->link_before ?? '' ) . $title . ( $args->link_after ?? '' );
+		$output .= $indent . '<li' . $class_names . '>';
 
-        if ( in_array( 'menu-item-has-children', $classes, true ) && $depth === 0 ) {
-            $item_output .= '<span class="cp-dropdown-arrow" aria-hidden="true">▾</span>';
-        }
+		$atts           = [];
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target ) ? $item->target : '';
+		$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
+		$atts['href']   = ! empty( $item->url ) ? $item->url : '';
 
-        $item_output .= '</a>';
-        $item_output .= $args->after ?? '';
+		$atts       = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+		$attributes = '';
 
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-    }
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value       = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+		$title = apply_filters( 'the_title', $item->title, $item->ID );
+		$title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+		$item_output  = $args->before ?? '';
+		$item_output .= '<a class="lct-menu__link"' . $attributes . '>';
+		$item_output .= ( $args->link_before ?? '' ) . $title . ( $args->link_after ?? '' );
+		$item_output .= '</a>';
+
+		if ( in_array( 'menu-item-has-children', $classes, true ) ) {
+			$item_output .= '<button class="lct-menu__toggle" aria-expanded="false" aria-label="' . esc_attr__( 'Toggle submenu', 'listingcore-theme' ) . '">';
+			$item_output .= '<span class="lct-menu__toggle-icon" aria-hidden="true"></span>';
+			$item_output .= '</button>';
+		}
+
+		$item_output .= $args->after ?? '';
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+
+	/**
+	 * Ends the element output.
+	 *
+	 * @param string   $output Passed by reference. Used to append additional content.
+	 * @param WP_Post  $item   Page data object.
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
+	public function end_el( &$output, $item, $depth = 0, $args = null ) {
+		$output .= "</li>\n";
+	}
 }
